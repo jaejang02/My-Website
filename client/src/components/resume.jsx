f6e02072a3adf098231107fb67d57f8da3cd8a42
Jae Hong Jang <jaepongjang@gmail.com>
2026-06-04 02:52:33 -0700

    Fix My Resume

--- client/src/components/resume.jsx
+++ client/src/components/resume.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+
 import './style.css';
 const MyResume = () => {
     return(
         <div className ='pdf'>
             <iframe
-                src={`/Jae_Hong_Jang_Resume.pdf`}
+                src={`${import.meta.env.BASE_URL}Jae_Hong_Jang_Resume.pdf`}
                 width ="100%"
                 height ="1200px"
                 loading = "lazy"
